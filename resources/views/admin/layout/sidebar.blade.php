815a61b141fdf3bf7f59d19ba8a835d699ff9afb
Olexandr <shapovalik26@gmail.com>
2023-11-02 22:44:22 +0200

    update from permisiions siedbar

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -2,10 +2,10 @@
     <nav class="sidebar-nav">
         <ul class="nav">
             <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.content') }}</li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/categories') }}"><i class="nav-icon icon-ghost"></i> {{ trans('admin.category.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/posts') }}"><i class="nav-icon icon-diamond"></i> {{ trans('admin.post.title') }}</a></li>
-           <li class="nav-item"><a class="nav-link" href="{{ url('admin/comments') }}"><i class="nav-icon icon-globe"></i> {{ trans('admin.comment.title') }}</a></li>
-           {{-- Do not delete me :) I'm used for auto-generation menu items --}}
+            @can('admin.category.index')<li class="nav-item"><a class="nav-link" href="{{ url('admin/categories') }}"><i class="nav-icon icon-map"></i> {{ trans('admin.category.title') }}</a></li>@endcan
+            @can('admin.post.index')<li class="nav-item"><a class="nav-link" href="{{ url('admin/posts') }}"><i class="nav-icon icon-power"></i> {{ trans('admin.post.title') }}</a></li>@endcan
+            @can('admin.comment.index')<li class="nav-item"><a class="nav-link" href="{{ url('admin/comments') }}"><i class="nav-icon icon-globe"></i> {{ trans('admin.comment.title') }}</a></li>@endcan
+            {{-- Do not delete me :) I'm used for auto-generation menu items --}}
 
             <li class="nav-title">{{ trans('brackets/admin-ui::admin.sidebar.settings') }}</li>
             @can('admin.admin-user.index')<li class="nav-item"><a class="nav-link" href="{{ url('admin/admin-users') }}"><i class="nav-icon icon-user"></i> {{ __('Manage access') }}</a></li>@endcan
@@ -17,3 +17,4 @@
     </nav>
     <button class="sidebar-minimizer brand-minimizer" type="button"></button>
 </div>
+
